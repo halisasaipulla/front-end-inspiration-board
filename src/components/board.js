c5ac42b66afe6dc2902931c9da564e37a6081b9c
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-06-30 15:50:08 -0700

    added styling to the board-sec className

--- src/components/board.js
+++ src/components/board.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './board.css';
+
+
+
 const Board = ({ index, active, onClick, currentBoard, board }) => {
     return (
-      <div onClick={() => {onClick(); currentBoard(board)}} className={active ? "active title" : "title"}>
+      <div onClick={() => {onClick(); currentBoard(board)}} className={active ? "active title box-size" : "title box-size"}>
         {index}. {board.title} - {board.owner}
       </div>
     );

5493179716e74485c9af830dea0c516586d090d1
lisa1501 <halisasaipulla@gmail.com>
2021-06-29 19:29:02 -0700

    completed board session

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
 import './App.css';
-// import BoardForm from './components/boardForm';
+import BoardForm from './components/boardForm';
 import Board from './components/board';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-// axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
-//     // ...
 const App = () => {
-    const boardsData=[
-        {'id': 44, 'owner': 'Melissa', 'title': 'Scissors'},
-        {'id': 45, 'owner': 'Halisa', 'title': 'Papers'},
-        {'id': 46, 'owner': 'Katrina', 'title': 'Rocks'}]
-
+    const [boardsData, setBoardsData] = useState([]);
     const [chosen, setChosen] = useState();
 
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
+        }).then((response) => {
+          setBoardsData(response.data);
+        })
+      }, []);
+    
     const boardsElements = boardsData.map((board) => {
         return (
             <li>              
                 <Board
-                  key={board.id}
-                  title={board.title}
-                  owner={board.owner}
-                  active={board.title === chosen}
-                  onClick={() => setChosen(board.title)}
+                    key={board.id}
+                    title={board.title}
+                    owner={board.owner}
+                    active={board.title === chosen}
+                    onClick={() => setChosen(board.title)}
                 />
             </li>);
+        }
+    );
+    
+    const createNewBoard = (newBoard) => {
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards`, newBoard).then((response) => {
+            console.log("Response:", response.data.board);
+            const boards = [...boardsData];
+            boards.push(response.data.board);
+            setBoardsData(boards);
+        }).catch((error) => {
+            console.log('Error:', error);
+            alert('Couldn\'t create a new board.');
         });
+    }
      
     return (
-        <div>
+        <div className='board-header'>
             <h1>Inspiration Board</h1>
-            <section>
-                <h2>Boards</h2>
-                <ol className="boards__list">
-                    {boardsElements}
-                </ol>
-            </section> 
-            <section>
-                <h2>Create a New Board</h2>
-                {/* <BoardForm></BoardForm> */}
-                {/* {isBoardFormVisible ? <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm> : ''} */}
-                {/* <span onClick={toggleNewBoardForm} className='new-board-form__toggle-btn'>{isBoardFormVisible ? 'Hide New Board Form' : 'Show New Board Form'}</span> */}
+            <section className='board-sec'>
+                <div>
+                    <h2>Boards</h2>
+                    <ol className="board-list">
+                        {boardsElements}
+                    </ol>
+                </div>
+                <div>
+                    <h2>Create a New Board</h2>
+                    <BoardForm createNewBoard={createNewBoard}></BoardForm>
+                </div>
             </section>
         </div>
     );

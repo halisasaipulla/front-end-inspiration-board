2b2f831267406fbabfb6743878ecfe717ec393ab
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-07-01 14:13:30 -0700

    additional css changes

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,5 @@
 import './card.css';
 
-<<<<<<< HEAD
 const Card = ({ card, deleteCard, likeCard }) => {
     return (
     <div className='card'>
@@ -9,14 +8,6 @@ const Card = ({ card, deleteCard, likeCard }) => {
             <span className='card-bottom'>{card.likes_count}</span>
             <span onClick={() => {likeCard(card)}} className='card-bottom'>❤️</span>
             <span onClick={() => {deleteCard(card)}} className='card-bottom'>🗑</span>
-=======
-
-const Card = () => {
-    return (<div className='note'>
-        <span>Our first card!</span>
-        <div className="note-footer">
-            <small>30/06/2021</small>
->>>>>>> 334636b47eb0eb57ba1867ad363852e23d374c0f
         </div>
     </div>);
     
